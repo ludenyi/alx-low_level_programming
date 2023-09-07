060669f031d8405c34ef701ba79dc59e8058669b
ludenyi <wanjirusharon832@gmail.com>
2023-09-07 13:20:41 -0700

    4. Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+/**
+ * main - A programme that prints a line with puts function
+ * Return: 0 (sucess)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
