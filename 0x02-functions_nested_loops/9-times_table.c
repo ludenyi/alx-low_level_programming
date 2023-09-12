ec5cbbf98d2ba229df32f033a631fcf0afa63684
ludenyi <wanjirusharon832@gmail.com>
2023-09-12 14:21:05 -0700

    9. Learn your times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main- check the code
+ *
+ * Return: Always 0
+ * times_table: prints the multiplication table for the number 9
+ * description:function prints multiplication table for the number 9
+ * from 0 to 9. The table is displayed in grid format
+ */
+void times_table(void)
+{
+/*function implementation goes here*/
+}
