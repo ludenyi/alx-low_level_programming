4162fe6d54aae835b24c7bf78e6d1a9462d87e58
ludenyi <wanjirusharon832@gmail.com>
2023-09-14 13:53:07 -0700

    1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,1 +1,17 @@
-
+#include <stdio.h>
+/**
+ * main - causes an infinite loop
+ * Return: 0
+ */
+int main(void)
+{
+int i;
+print("infinite loop incoming :\n");
+i = 0;
+/*while (i < 10)*/
+/*{*/
+/*  puchar(i);*/
+/*}*/
+print("infinite loop avoided! \\o/\n");
+return (0);
+}
